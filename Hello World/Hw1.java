0bccd0604064a782b7be262cebd9aa250bcb1285
Shaik Mohd Ilyas <Shaik Mohd Ilyas@DESKTOP-4NM36G6>
2024-10-10 15:16:01 +0530

    p

---
+++ Hello World/Hw1.java
@@ -0,0 +1,17 @@
+
+public class Hw1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Hello World";
+		String nstr="";
+		char ch;
+		for(int i=0;i<str.length();i++) {
+			ch=str.charAt(i);
+			nstr = ch + nstr;
+		}
+		System.out.println(nstr);
+
+	}
+
+}
